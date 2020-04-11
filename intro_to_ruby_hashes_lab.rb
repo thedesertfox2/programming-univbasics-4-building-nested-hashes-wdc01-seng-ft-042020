5ad9df3715246e48a7812b8522f2f3bff44b1a12
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-11 16:13:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,11 +13,11 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   trains = {
-    :railroads => []
-      :pieces => 4
-      :rent_in_dollars => {}
-      :names => {}
-    ]
+    :railroads => {
+      {:pieces => 4}
+      {:rent_in_dollars => {}}
+      {:names => {}}
+    }
   }
   
   trains
