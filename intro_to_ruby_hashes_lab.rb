0ccab49f7c4ec64feff34138167fcdd008cd117b
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-11 16:19:22 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,16 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
+  trains = {
+    :railroads => {
+      :pieces => 4,
+      :rent_in_dollars => [
+        ],
+      :names => {}
+    }
+  }
+  
+  trains
 
 end
 
