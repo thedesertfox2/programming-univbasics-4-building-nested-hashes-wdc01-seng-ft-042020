cbbbb927aaf921ae6b016211ee22ca2fc815d39d
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-11 16:18:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,7 @@ def monopoly_with_second_tier
   # complexity.
   trains = {
     :railroads => {
-      :pieces => {4},
+      :pieces => 4,
       :rent_in_dollars => [],
       :names => []
     }
